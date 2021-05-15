d7590b8665541610117eeb330260afe684b1ce04
Kadi Kraman <hellokadi@gmail.com>
2021-05-15 16:01:17 +0100

    Upgrade RN to fix flipper

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 
 export const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Hello, world!</Text>
+      <Text>Hi there!</Text>
     </View>
   );
 };
@@ -12,5 +12,7 @@ export const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
